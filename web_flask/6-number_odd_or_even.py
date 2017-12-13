163bf15b3e9207f1ce5f2982e7de6dbc53831e04
Tanya Kryukova <ttkryukova@gmail.com>
2017-12-13 05:29:42 +0000

    first commit

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+import string
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+        return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+        return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+        return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text=None):
+        if text:
+                return 'Python %s' % text.replace('_', ' ')
+        return 'Python is cool'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+        return '%d is a number' % n
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+        return render_template('5-number.html', num=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_even(n):
+        div_2 = 'even'
+        if n % 2 != 0:
+                div_2 = 'odd'
+        return render_template(
+                '6-number_odd_or_even.html', num=n, even_odd=div_2)
+
+if __name__ == '__main__':
+        # app.run() defaults to listening on port 5000
+        app.run(host="0.0.0.0")
