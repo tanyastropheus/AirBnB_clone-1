af9c8d76678fbadf525c628e6bf6453e4cb930f6
Tanya Kryukova <ttkryukova@gmail.com>
2017-12-13 04:15:40 +0000

    first commit

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from flask import Flask
+import string
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+        return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+        return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+        return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+def python():
+        return 'Python is cool'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+        return 'Python %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+        # app.run() defaults to listening on port 5000
+        app.run(host="0.0.0.0")
